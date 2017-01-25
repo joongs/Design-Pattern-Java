58eb6766b9a06ed94cd527a141b3faefe3807b48
joongs <luck1004.kim@gmail.com>
2017-01-25 22:59:01 +0900

    Ready to setup strategy pattern with java and TDD.

---
+++ StrategyDesignePatternJava/src/test/strategy/pattern/TestMallardDuck.java
@@ -0,0 +1,14 @@
+package test.strategy.pattern;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMallardDuck {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
